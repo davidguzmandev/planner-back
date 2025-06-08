d1712f71153a09c4244554917f62455affd90397
davidruncode <daguz.91@gmail.com>
2025-06-07 23:09:50 -0400

    Sever.ts

---
+++ src/config/db.ts
@@ -0,0 +1,26 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    console.error('Database Url is not defined in the environment variables.');
+    process.exit(1);
+}
+
+const pool = new Pool({
+    connectionString: connectionString,
+});
+
+pool.on('connect', ()=> {
+    console.log('Connected to the database successfully.');
+})
+
+pool.on('remove', () => {
+    console.log('Database connection closed.');
+})
+
+pool.on('error', (err: Error, client:any) => {
+    console.error('Error, inactive client on the DB', err);
+})
